9b46a05c62c9d065c27ca8aee55cb07752f00921
PedroCoelhoSouza <pedrohcoelhosouza@gmail.com>
2026-03-10 11:15:32 -0300

    Adicionando códigos dos exercícios

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("Digite o primeiro número:"));
+let num2 = Number(prompt("Digite o segundo número:"));
+
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+console.log("Soma:", soma);
+console.log("Subtração:", sub);
+console.log("Multiplicação:", mult);
+console.log("Divisão:", div);
---
+++ media.js
@@ -0,0 +1,11 @@
+let n1 = Number(prompt("Nota 1:"));
+let n2 = Number(prompt("Nota 2:"));
+let n3 = Number(prompt("Nota 3:"));
+
+let media = (n1 + n2 + n3) / 3;
+
+if (media >= 7) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+}
